20e7181ba55372e36133ae6a947a627e593b6e16
Leo Devin <leo.devin@epita.fr>
2024-11-08 11:12:35 +0100

    vr unsupported

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,7 @@ const Index = () => {
   return (
     <div className="container">
       <Leva fill hideCopyButton />
-      <VRButton />
+      <ARButton />
       <Canvas className="canvas-container" shadows>
         <XR referenceSpace="local-floor">
           <PerspectiveCamera makeDefault position={cameraPosition} />
